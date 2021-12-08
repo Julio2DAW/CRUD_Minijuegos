bc57b6d40231d5dbc9302332bf29df689e1ab955
Julio Antonio Ramos Gago <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2021-12-08 21:10:14 +0100

    añadir niveles

--- index.php
+++ index.php
@@ -9,32 +9,7 @@
 	</head>
 	<body>
         <ul>
-            <li> <a href="clases/direc.php?op=lista"> LISTA NIVELES </a> </li>
-            <li> <a href="clases/direc.php?op=anadir"> AÑADIR NIVELES </a> </li>
+            <li><a href="clases/formulario/anadir.php"> AÑADIR NIVELES </a></li>
         </ul>
-        <?php
-            if (isset($_GET['op'])) 
-            {
-                switch ($_GET['op']) 
-                {
-                    case 'anadir':
-                        require('clases/anadir.php'); //Modulado, asi llamara al codigo que requiero para la lista
-                        break;
-
-                    case 'borrar':
-                        require('clases/borrar.php'); //Modulado, asi llamara al codigo que requiero para borrar
-                        break;
-
-                    case 'modificar':
-                        require('clases/modificar.php'); //Modulado, asi llamara al codigo que requiero para modificar un empleado
-                        break;
-                }
-            }
-            else
-            {
-                require('clases/lista.php'); //Modulado, asi llamara al codigo que requiero para la lista
-            }
-            
-        ?>
 	</body>
 </html>
